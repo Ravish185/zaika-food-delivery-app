0bb4c6626a8d4bc807bef944edd6787952afaa22
Ravish185 <ravishupadhyay.18@gmail.com>
2025-07-31 21:35:04 +0530

    Create Main.java

---
+++ box,type src/Main.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Zaika - Food Delivery App");
+        // Entry point of the app
+    }
+}
