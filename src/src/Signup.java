0fb02274212382954b21c5a92b637e7f25e2a38e
Ravish185 <ravishupadhyay.18@gmail.com>
2025-07-31 21:46:58 +0530

    Create Signup.java

---
+++ src/src/Signup.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Signup {
+    public void registerUser(String username, String password, String email) {
+        // Simulate user registration logic
+        System.out.println("User registered: " + username);
+    }
+}
