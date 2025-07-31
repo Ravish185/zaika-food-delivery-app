8cc058e24692b95142d8d8c14e26c5823bfa025c
Ravish185 <ravishupadhyay.18@gmail.com>
2025-07-31 21:45:51 +0530

    Create Login.java

---
+++ src/Login.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Login {
+    public boolean authenticateUser(String username, String password) {
+        // Simulate login authentication
+        return username.equals("user") && password.equals("pass");
+    }
+}
